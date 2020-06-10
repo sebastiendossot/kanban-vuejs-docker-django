8b4f183ec5bd7e16951278f779bd2b9d4e2ec6a8
Sebastien Dossot <sebastien.dossot@gmail.com>
2020-06-10 15:08:11 +0200

    Partly working version to be deployed on prod

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qt&m@c4ooedo-@4b0)4s3wn^+80g!1qu^g+rz5=7bl^y16!_tq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sebastiendossot.fr', '127.0.0.1', 'localhost']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -153,4 +153,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
